99ccdd26cdac654ea8e9dff499098ece642671ba
Niki Zadeh <niki.zadeh@noaa.gov>
2022-03-02 12:23:20 -0500

    An alternate fix to class(*) issues with FMS 2022-01
    - This update is an alternate to PR#66 to fix the issues with passing
      read arguments to subroutines receiving class(*)
    - This tries to show that there are no inherent issues with passing a
      real and receiving it as class(*).
      Rather the root cause of the issues is some of these arguments are
      optional and are being passed to FMS even thought they are not present!
      Then they are trapped in FMS diag_manager inside a SELECT TYPE statement
      and the compiler is not smart enough to know that they are absent and bombs. 
    - Added the same fix for infra/FMS2
    - Fixed trailing spaces

--- config_src/infra/FMS2/MOM_diag_manager_infra.F90
+++ config_src/infra/FMS2/MOM_diag_manager_infra.F90
@@ -289,7 +289,7 @@ logical function send_data_infra_2d(diag_field_id, field, is_in, ie_in, js_in, j
   character(len=*),        optional, intent(out) :: err_msg !< A log indicating the status of the post upon
                                                          !! returning to the calling routine
 
-  if(present(rmask) .or. present(weight)) then                                                         
+  if(present(rmask) .or. present(weight)) then
     send_data_infra_2d = send_data_fms(diag_field_id, field, time=time, is_in=is_in, js_in=js_in, mask=mask, &
                                 rmask=rmask, ie_in=ie_in, je_in=je_in, weight=weight, err_msg=err_msg)
   else
