793a4f407e079fbc56512af42793447b33a6fe14
Robert Hallberg <Robert.Hallberg@noaa.gov>
2022-12-18 09:18:55 -0500

    Avoid using the unscaled argument to MOM_get_param
      Avoid using the unscaled argument to MOM_get_param in 5 places in 4 files, and
    made related changes to avoid using some unscaled variables:
    
    - Replace Hmix_min_m (in [m]) with Hmix_min_z (in [Z ~> m]) in bulkmixedlayer_init
      and then set CS%Hmix_min by rescaling Hmix_min_z to [H ~> m or kg m-2].
    
    - Replace Hmix_m (in [m]) with Hmix_z (in [Z ~> m]) in vertvisc_init, and
      then set CS%Hmix by rescaling Hmix_z to [H ~> m or kg m-2].
    
    - Add a separate unlogged and unscaled get_param call for FLUXCONST for
      flux_const_default in the solo_driver surface_forcing_init
    
    - Do not rescale FLUXCONST when it is read in the FMS_cap surface_forcing_init.
    
    - The Flux_const element of the FMS_cap surface_forcing_CS was not actually
      being used apart from setting a default for Flux_const_temp and
      Flux_const_salt, so it was replaced with a local variable in
      surface_forcing_init.
    
    - Added comments better describing the internal variables in
      surface_forcing_init.
    
    All answers and output are bitwise identical.

--- src/parameterizations/vertical/MOM_bulk_mixed_layer.F90
+++ src/parameterizations/vertical/MOM_bulk_mixed_layer.F90
@@ -3363,7 +3363,7 @@ subroutine bulkmixedlayer_init(Time, G, GV, US, param_file, diag, CS)
   real :: BL_detrain_time_dflt ! The default value for BUFFER_LAY_DETRAIN_TIME [s]
   real :: omega_frac_dflt  ! The default value for ML_OMEGA_FRAC [nondim]
   real :: ustar_min_dflt   ! The default value for BML_USTAR_MIN [Z T-1 ~> m s-1]
-  real :: Hmix_min_m       ! The unscaled value of HMIX_MIN [m]
+  real :: Hmix_min_z       ! The default value of HMIX_MIN [Z ~> m]
   integer :: isd, ied, jsd, jed
   logical :: use_temperature, use_omega
   isd = G%isd ; ied = G%ied ; jsd = G%jsd ; jed = G%jed
@@ -3419,10 +3419,10 @@ subroutine bulkmixedlayer_init(Time, G, GV, US, param_file, diag, CS)
   call get_param(param_file, mdl, 'VON_KARMAN_CONST', CS%vonKar, &
                  'The value the von Karman constant as used for mixed layer viscosity.', &
                  units='nondim', default=0.41)
-  call get_param(param_file, mdl, "HMIX_MIN", CS%Hmix_min, &
+  call get_param(param_file, mdl, "HMIX_MIN", Hmix_min_Z, &
                  "The minimum mixed layer depth if the mixed layer depth "//&
-                 "is determined dynamically.", units="m", default=0.0, scale=GV%m_to_H, &
-                 unscaled=Hmix_min_m)
+                 "is determined dynamically.", units="m", default=0.0, scale=US%m_to_Z)
+  CS%Hmix_min = GV%Z_to_H * Hmix_min_Z
 
   call get_param(param_file, mdl, "LIMIT_BUFFER_DETRAIN", CS%limit_det, &
                  "If true, limit the detrainment from the buffer layers "//&
@@ -3466,7 +3466,7 @@ subroutine bulkmixedlayer_init(Time, G, GV, US, param_file, diag, CS)
   call get_param(param_file, mdl, "DEPTH_LIMIT_FLUXES", CS%H_limit_fluxes, &
                  "The surface fluxes are scaled away when the total ocean "//&
                  "depth is less than DEPTH_LIMIT_FLUXES.", &
-                 units="m", default=0.1*Hmix_min_m, scale=GV%m_to_H)
+                 units="m", default=0.1*US%Z_to_m*Hmix_min_z, scale=GV%m_to_H)
   call get_param(param_file, mdl, "OMEGA", CS%omega, &
                  "The rotation rate of the earth.", &
                  default=7.2921e-5, units="s-1", scale=US%T_to_s)
